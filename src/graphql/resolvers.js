210609a92ec130eb4b5bb376d6e9a91c43c4585f
Damian <damianjjoniec@gmail.com>
2019-09-27 08:32:07 +0100

    adding items with apollo 1

--- src/graphql/resolvers.js
+++ src/graphql/resolvers.js
@@ -1,14 +1,26 @@
 import { gql } from 'apollo-boost'
+import { addItemToCart } from '../redux/cart/cart.utils'
 
 export const typeDefs = gql`
+    extend type Item {
+        quantity: Int
+    }
+
     extend type Mutation {
-        ToggleCartHidden: Boolean!
+        ToggleCartHidden: Boolean!,
+        AddItemToCart(item: Item!): [Item]!
     }
 `
 
 const GET_CART_HIDDEN = gql`
     {
         cartHidden @client
+    }
+`
+
+const GET_CART_ITEMS = gql`
+    {
+        cartItems @client
     }
 `
 
@@ -24,6 +36,18 @@ export const resolvers = {
         data: { cartHidden: !cartHidden }
       })
       return !cartHidden
+    },
+    AddItemToCart: (_root, { item }, { cache }) => {
+      const { cartItems } = cache.readQuery({
+        query: GET_CART_ITEMS
+      })
+      const newCartItems = addItemToCart(cartItems, item)
+      
+      cache.writeQuery({
+        query: GET_CART_ITEMS,
+        data: { cartItems: newCartItems }
+      })
+      return newCartItems
     }
   }
 }
